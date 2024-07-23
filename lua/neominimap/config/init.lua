1a815e90d02b307e7b0ace806dac50ae75bb42be
Isrothy <isrothy@gmail.com>
2024-07-23 23:09:09 +0800

    feat: Use vim.validate to validate user config

--- lua/neominimap/config/init.lua
+++ lua/neominimap/config/init.lua
@@ -4,8 +4,6 @@ local M = {}
 
 local user_config = type(vim.g.neominimap) == "function" and vim.g.neominimap() or vim.g.neominimap or {}
 
-validator.validate_user_config(user_config)
-
 if type(user_config.log_level) == "string" then
     user_config.log_level = vim.log.levels[user_config.log_level]
 end
@@ -13,11 +11,16 @@ if type(user_config.notification_level) == "string" then
     user_config.notification_level = vim.log.levels[user_config.notification_level]
 end
 
-local confg = vim.tbl_deep_extend("force", require("neominimap.config.internal").default_config, user_config)
+local cfg = vim.tbl_deep_extend("force", require("neominimap.config.internal").default_config, user_config)
+
+local ok, err = validator.validate_config(cfg)
+if not ok then
+    vim.notify("neominimap: invalid config: " .. err, vim.log.levels.ERROR)
+end
 
 ---@return Neominimap.InternalConfig
 M.get = function()
-    return confg
+    return cfg
 end
 
 return M
--- lua/neominimap/config/validator.lua
+++ lua/neominimap/config/validator.lua
@@ -1,248 +1,73 @@
 local M = {}
 
----@param value any
----@param expected_type string | string[]
----@param name string
----@return boolean
-local function validate_type(value, expected_type, name)
-    if type(expected_type) == "string" then
-        if value ~= nil and type(value) ~= expected_type then
-            vim.notify(
-                string.format("Invalid type for %s: expected %s, got %s", name, expected_type, type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    else
-        if value ~= nil and not vim.tbl_contains(expected_type, type(value)) then
-            vim.notify(
-                string.format(
-                    "Invalid type for %s: expected one of %s, got %s",
-                    name,
-                    vim.inspect(expected_type),
-                    type(value)
-                ),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
+---@param path string The path to the field being validated
+---@param tbl table The table to validate
+---@see vim.validate
+---@return boolean is_valid
+---@return string|nil error_message
+local function validate_path(path, tbl)
+    local ok, err = pcall(vim.validate, tbl)
+    return ok, err and path .. "." .. err
 end
 
----@param value any
----@param expected_type_table table
----@param name string
----@return boolean
-local function validate_table(value, expected_type_table, name)
-    if value ~= nil then
-        for k, v in pairs(expected_type_table) do
-            if not validate_type(value[k], v, name .. "." .. k) then
+---@param f fun(x:any):boolean
+---@return fun(arr:table):boolean
+local function is_array_of(f)
+    return function(tbl)
+        if type(tbl) ~= "table" then
+            return false
+        end
+        for _, v in ipairs(tbl) do
+            if not f(v) then
                 return false
             end
         end
-    end
-    return true
-end
-
----@param value any
----@param expected_values any[]
----@param name string
----@return boolean
-local function validate_enum(value, expected_values, name)
-    if value ~= nil then
-        if not vim.tbl_contains(expected_values, value) then
-            vim.notify(
-                string.format(
-                    "Invalid value for %s: expected one of %s, got %s",
-                    name,
-                    vim.inspect(expected_values),
-                    value
-                ),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
----@param value any
----@param expected_type string | string[]
----@param name string
----@return boolean
-local function validate_array(value, expected_type, name)
-    if value ~= nil then
-        for _, v in ipairs(value) do
-            if not validate_type(v, expected_type, name) then
-                return false
-            end
-        end
-    end
-    return true
-end
-
----@param value any
----@return boolean
-local function validate_log_level(value)
-    if value ~= nil then
-        if type(value) == "string" then
-            return validate_enum(value, vim.tbl_keys(vim.log.levels), "log_level")
-        elseif type(value) == "number" then
-            return validate_enum(value, vim.tbl_values(vim.log.levels), "log_level")
-        else
-            vim.notify(
-                string.format("Invalid type for log_level: expected string or number, got %s", type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
----@param value any
----@return boolean
-local function validate_notification_level(value)
-    if value ~= nil then
-        if type(value) == "string" then
-            return validate_enum(value, vim.tbl_keys(vim.log.levels), "notification_level")
-        elseif type(value) == "number" then
-            return validate_enum(value, vim.tbl_values(vim.log.levels), "notification_level")
-        else
-            vim.notify(
-                string.format("Invalid type for notification_level: expected string or number, got %s", type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
----@param value any
----@return boolean
-local function validate_diagnostic(value)
-    if value ~= nil then
-        if type(value) == "table" then
-            return true
-                and validate_type(value.enabled, "boolean", "diagnostic.enabled")
-                and validate_enum(value.severity, vim.diagnostic.severity, "diagnostic.severity")
-                and validate_table(value.priority, {
-                    ERROR = "number",
-                    WARN = "number",
-                    INFO = "number",
-                    HINT = "number",
-                }, "diagnostic.priority")
-        else
-            vim.notify(
-                string.format("Invalid type for diagnostic: expected table, got %s", type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
----@param value any
----@return boolean
-local function validate_treesitter(value)
-    if value ~= nil then
-        if type(value) == "table" then
-            return true
-                and validate_type(value.enabled, "boolean", "treesitter.enabled")
-                and validate_type(value.priority, "number", "treesitter.priority")
-        else
-            vim.notify(
-                string.format("Invalid type for treesitter: expected table, got %s", type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
----@param value any
----@return boolean
-local function validate_margin(value)
-    if value ~= nil then
-        if type(value) == "table" then
-            return true
-                and validate_type(value.right, "number", "margin.right")
-                and validate_type(value.top, "number", "margin.top")
-                and validate_type(value.bottom, "number", "margin.bottom")
-        else
-            vim.notify(
-                string.format("Invalid type for margin: expected table, got %s", type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
-local function validate_winopt(value)
-    if value ~= nil then
-        if type(value) == "function" then
-            return true
-        elseif type(value) == "table" then
-            return true
-        else
-            vim.notify(
-                string.format("Invalid type for winopt: expected function or table, got %s", type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
-local function validate_bufopt(value)
-    if value ~= nil then
-        if type(value) == "function" then
-            return true
-        elseif type(value) == "table" then
-            return true
-        else
-            vim.notify(
-                string.format("Invalid type for bufopt: expected function or table, got %s", type(value)),
-                vim.log.levels.ERROR
-            )
-            return false
-        end
-    end
-    return true
-end
-
----@param config any
----@return boolean
-M.validate_user_config = function(config)
-    if config ~= nil then
         return true
-            and validate_type(config.auto_enable, "boolean", "auto_enable")
-            and validate_log_level(config.log_level)
-            and validate_notification_level(config.notification_level)
-            and validate_array(config.exclude_filetypes, "string", "exclude_filetypes")
-            and validate_array(config.exclude_buftypes, "string", "exclude_buftypes")
-            and validate_type(config.buf_filter, "function", "buf_filter")
-            and validate_type(config.win_filter, "function", "win_filter")
-            and validate_type(config.minimap_width, "number", "minimap_width")
-            and validate_type(config.x_multiplier, "number", "x_multiplier")
-            and validate_type(config.y_multiplier, "number", "y_multiplier")
-            and validate_type(config.delay, "number", "delay")
-            and validate_type(config.z_index, "number", "z_index")
-            and validate_type(config.window_border, { "string", "table" }, "window_border")
-            and validate_diagnostic(config.diagnostic)
-            and validate_treesitter(config.treesitter)
-            and validate_margin(config.margin)
-            and validate_winopt(config.winopt)
-            and validate_bufopt(config.bufopt)
     end
-    return true
+end
+
+---@type fun(x:any):boolean
+local is_string_array = is_array_of(function(x)
+    return type(x) == "string"
+end)
+
+---@param cfg any
+---@return boolean
+M.validate_config = function(cfg)
+    return validate_path("vim.g.neominimap", {
+        auto_enable = { cfg.auto_enable, "boolean" },
+        log_level = { cfg.log_level, "number" },
+        notification_level = { cfg.notification_level, "number" },
+        log_path = { cfg.log_path, "string" },
+        exclude_filetypes = { cfg.exclude_filetypes, is_string_array, "list of string" },
+        exclude_buftypes = { cfg.exclude_buftypes, is_string_array, "list of string" },
+        buf_filter = { cfg.buf_filter, "function" },
+        win_filter = { cfg.win_filter, "function" },
+        max_minimap_height = { cfg.max_win_height, "number", true },
+        minimap_width = { cfg.minimap_width, "number", true },
+        x_multiplier = { cfg.x_multiplier, "number", true },
+        y_multiplier = { cfg.y_multiplier, "number", true },
+        delay = { cfg.delay, "number", true },
+
+        ["diagnostic.enabled"] = { cfg.diagnostic.enabled, "boolean", true },
+        ["diagnostic.severity"] = { cfg.diagnostic.severity, "number", true },
+        ["diagnostic.priority.ERROR"] = { cfg.diagnostic.priority.ERROR, "number", true },
+        ["diagnostic.priority.WARN"] = { cfg.diagnostic.priority.WARN, "number", true },
+        ["diagnostic.priority.INFO"] = { cfg.diagnostic.priority.INFO, "number", true },
+        ["diagnostic.priority.HINT"] = { cfg.diagnostic.priority.HINT, "number", true },
+
+        ["treesitter.enabled"] = { cfg.treesitter.enabled, "boolean", true },
+        ["treesitter.priority"] = { cfg.treesitter.priority, "number", true },
+
+        ["margin.right"] = { cfg.margin.right, "number", true },
+        ["margin.top"] = { cfg.margin.top, "number", true },
+        ["margin.bottom"] = { cfg.margin.bottom, "number", true },
+
+        z_index = { cfg.z_index, "number", true },
+        window_border = { cfg.window_border, { "string", "table" }, true },
+        winopt = { cfg.winopt, { "table", "function" }, true },
+        bufopt = { cfg.bufopt, { "table", "function" }, true },
+    })
 end
 
 return M
