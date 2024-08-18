e9f4638da9268684bcecda330e2c15c0ebfcc9b8
Isrothy <isrothy@gmail.com>
2024-08-18 10:51:14 -0400

    refactor: Introduce focus_cmd interface

--- lua/neominimap/command/focus.lua
+++ lua/neominimap/command/focus.lua
@@ -1,28 +1,11 @@
 local api = vim.api
 
+---@class Neominimap.Command.Focus.Handler
+---@field focus fun(winid:integer)
+---@field unfocus fun(mwinid:integer)
+---@field toggleFocus fun(winid:integer)
+
 local M = {}
-
----@param winid integer
-local focus = function(winid)
-    vim.schedule(function()
-        local window = require("neominimap.window")
-        if not window.focus(winid) then
-            local logger = require("neominimap.logger")
-            logger.notify("Minimap can not be focused for current window", vim.log.levels.ERROR)
-        end
-    end)
-end
-
----@param mwinid integer
-local unfocus = function(mwinid)
-    vim.schedule(function()
-        local window = require("neominimap.window")
-        if not window.unfocus(mwinid) then
-            local logger = require("neominimap.logger")
-            logger.notify("Minimap can not be unfocused for current window", vim.log.levels.ERROR)
-        end
-    end)
-end
 
 ---@type table<string, Neominimap.Subcommand>
 M.subcommand_tbl = {
@@ -30,29 +13,21 @@ M.subcommand_tbl = {
         impl = function(args, opts)
             local winid = api.nvim_get_current_win()
             local window = require("neominimap.window")
-            if not window.is_minimap_window(winid) then
-                focus(winid)
-            end
+            window.focus_cmds.focus(winid)
         end,
     },
     unfocus = {
         impl = function(args, opts)
             local winid = api.nvim_get_current_win()
             local window = require("neominimap.window")
-            if window.is_minimap_window(winid) then
-                unfocus(winid)
-            end
+            window.focus_cmds.unfocus(winid)
         end,
     },
     toggleFocus = {
         impl = function(args, opts)
             local winid = api.nvim_get_current_win()
             local window = require("neominimap.window")
-            if window.is_minimap_window(winid) then
-                unfocus(winid)
-            else
-                focus(winid)
-            end
+            window.focus_cmds.toggleFocus(winid)
         end,
     },
 }
