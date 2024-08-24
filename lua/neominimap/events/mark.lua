4c326414f5993e8806606385751b5ceed831bd26
Jiahe Jiang <61075605+Isrothy@users.noreply.github.com>
2024-08-24 11:13:51 -0400

    create mark event

---
+++ lua/neominimap/events/mark.lua
@@ -0,0 +1,47 @@
+--- This code is adapted from the Satellite.nvim project by Lewis6991.
+--- Satellite.nvim is available under the MIT License at https://github.com/lewis6991/satellite.nvim.
+
+--- This module implements a user autocmd for Mark events
+
+local util = require("satellite.util")
+
+local api, fn = vim.api, vim.fn
+
+--- @param data any
+local function exec_autocmd(data)
+    api.nvim_exec_autocmds("User", {
+        pattern = "Mark",
+        data = data,
+    })
+end
+
+--- @param key string
+--- @param m string mark name
+local function mark_set_keymap(key, m)
+    local mkey = key .. m
+    if fn.maparg(mkey) == "" then
+        vim.keymap.set({ "n", "x" }, mkey, function()
+            exec_autocmd({ key = mkey })
+            return mkey
+        end, { unique = true, expr = true })
+    end
+end
+
+--- @param key string
+return function(key)
+    for code = string.byte("A"), string.byte("Z") do
+        mark_set_keymap(key, string.char(code))
+    end
+
+    for code = string.byte("a"), string.byte("z") do
+        mark_set_keymap(key, string.char(code))
+    end
+
+    local group = api.nvim_create_augroup("NeominimapMark", {})
+
+    for _, cmd in ipairs({ "k", "mar", "delm" }) do
+        util.on_cmd(cmd, group, function()
+            exec_autocmd({ cmd = cmd })
+        end)
+    end
+end
