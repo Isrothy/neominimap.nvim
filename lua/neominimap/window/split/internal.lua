4e53d0531e9ebbcee629cec95c80e0b110ddd614
Alex <silvercircle@gmail.com>
2025-03-19 02:35:04 +0100

    fix formatting (long line)

--- lua/neominimap/window/split/internal.lua
+++ lua/neominimap/window/split/internal.lua
@@ -130,7 +130,13 @@ M.create_minimap_window_in_current_tab = function()
         ["aboveleft"]   = "aboveleft",
         ["rightbelow"]  = "rightbelow"
     }
-    vim.cmd(string.format("noau vertical %s %dsplit", dir_tbl[config.split.direction] or "botright", config:get_minimap_width()))
+    vim.cmd(
+        string.format(
+            "noau vertical %s %dsplit",
+            dir_tbl[config.split.direction] or "botright",
+            config:get_minimap_width()
+        )
+    )
     local mwinid = vim.api.nvim_get_current_win()
     util.noautocmd(api.nvim_set_current_win)(winid)
 
